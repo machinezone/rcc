46ad10b909458d901bd2d62cabad61200922a0f2
Benjamin Sergeant <bsergean@gmail.com>
2020-04-17 14:05:13 -0700

    keyspace / print top N keys

--- rcc/runner/keyspace.py
+++ rcc/runner/keyspace.py
@@ -26,7 +26,10 @@ from rcc.plot import asciiPlot
 @click.option('--quiet', '-q', is_flag=True)
 @click.option('--count', '-c', default=-1)
 @click.option('--monitor', '-m', is_flag=True)
-def keyspace(redis_urls, port, redis_password, timeout, path, quiet, count, monitor):
+@click.option('--max_keys', '-k', default=50)
+def keyspace(
+    redis_urls, port, redis_password, timeout, path, quiet, count, monitor, max_keys
+):
     '''Analyze the keyspace'''
 
     keySpace = asyncio.run(
@@ -42,9 +45,12 @@ def keyspace(redis_urls, port, redis_password, timeout, path, quiet, count, moni
     weights = keySpace.keys
     writeWeightsToCsv(weights, path)
 
-    # print key access by redis node
-    # for node, access in sorted(result['nodes'].items()):
-    #    print(node, access)
+    temp = [(weight, key) for key, weight in keySpace.keys.items()]
+    temp.sort()
+    temp.reverse()
+    temp = temp[:max_keys]
+    keys = {v: k for k, v in temp}
 
+    asciiPlot(f'Top {max_keys} keys', keys)
     asciiPlot('Commands', keySpace.commands)
     asciiPlot('Nodes', keySpace.nodes)
