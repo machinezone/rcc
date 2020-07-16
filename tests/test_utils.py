9226446ab83703441dd1bc68c30bf814f1a1d4c5
Benjamin Sergeant <bsergean@gmail.com>
2020-07-15 22:57:38 -0700

    add a find free port routine for unittest

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,6 +4,7 @@ Copyright (c) 2020 Machine Zone, Inc. All rights reserved.
 '''
 
 import asyncio
+import socket
 from rcc.client import RedisClient
 
 
@@ -45,3 +46,28 @@ async def getSupportedCommands(client):
 async def isCommandSupported(client, cmd):
     commands = await getSupportedCommands(client)
     return cmd in commands
+
+
+def findFreePort(
+    interface='127.0.0.1', socket_family=socket.AF_INET, socket_type=socket.SOCK_STREAM
+):
+    """
+    Ask the platform to allocate a free port on the specified interface, then
+    release the socket and return the address which was allocated.
+
+    Copied from ``twisted.internet.test.connectionmixins.findFreePort``.
+
+    :param bytes interface: The local address to try to bind the port on.
+    :param int socket_family: The socket family of port.
+    :param int socket_type: The socket type of the port.
+
+    :return: A two-tuple of address and port, like that returned by
+        ``socket.getsockname``.
+    """
+    address = socket.getaddrinfo(interface, 0)[0][4]
+    probe = socket.socket(socket_family, socket_type)
+    try:
+        probe.bind(address)
+        return probe.getsockname()
+    finally:
+        probe.close()
